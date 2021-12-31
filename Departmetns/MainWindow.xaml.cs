d90f3c0058d04f6b681e7dfced275bc4650936ec
Андрей Заярный <zaazaa@yandex.ru>
2021-12-31 21:38:03 +0300

    Добавьте файлы проекта.

---
+++ Departmetns/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Departmetns.Model;
+
+namespace Departmetns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Departmetns.Model.Department departmetns = new Model.Department("Информационный");
+            //departmetns.Employees.Add(new Model.Employee("Иванов Иван", 33));
+            //departmetns.Employees.Add(new Model.Employee("Петров Петр", 35));
+            //departmetns.Employees.Add(new Model.Employee("Сидоров Андрей", 43));
+
+            //departmetns.WriteToXML("deparments.xml");
+            Department departments = Department.LoadFromXML("deparments.xml");
+            lbPeople.ItemsSource = departments.Employees;
+        }
+
+        private void lbPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tbName.Text = (lbPeople.SelectedItem as Employee).Name;
+            tbAge.Text = (lbPeople.SelectedItem as Employee).Age.ToString();
+        }
+    }
+}
---
+++ Departmetns/Model/Model.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Departmetns.Model
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+
+        public Employee(string Name, int Age)
+        {
+            this.Age = Age;
+            this.Name = Name;
+        }
+
+        public Employee()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+    public class Department
+    {
+        public Department(string Name)
+        {
+            this.Name = Name;
+            Employees = new List<Employee>();
+
+        }
+
+        public Department()
+        {
+
+        }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void WriteToXML(string filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Department));
+            StreamWriter streamWriter = new StreamWriter(filename);
+
+            xmlSerializer.Serialize(streamWriter,this);
+
+            streamWriter.Close();
+        }
+
+        static public Department LoadFromXML(string filename)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Department));
+            StreamReader streamReader = new StreamReader(filename);
+            Department department = new Department();
+            department=(xmlSerializer.Deserialize(streamReader) as Department);
+
+            streamReader.Close();
+            return department;
+
+        }
+    }
+}
