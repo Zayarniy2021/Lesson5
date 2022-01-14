3e1c4b7da87e08a33d2b4f30ea107d1582cddabc
Андрей Заярный <zaazaa@yandex.ru>
2022-01-15 00:32:13 +0300

    Lesson7

---
+++ ADONET_WPF/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADONET_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable dt;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Sasha\Desktop\ExampleDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            SqlCommand command = new SqlCommand("SELECT ID, FIO, Birthday, Email, Phone FROM [dbo].[Table]", connection);
+            adapter.SelectCommand = command;
+            //insert
+            command = new SqlCommand(@"INSERT INTO [dbo].[Table] (FIO, Birthday, Email, Phone) VALUES (@FIO, @Birthday, @Email, @Phone); SET @ID = @@IDENTITY;", connection);
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@Birthday", SqlDbType.NVarChar, -1, "Birthday");
+            command.Parameters.Add("@Email", SqlDbType.NVarChar, 100, "Email");
+            command.Parameters.Add("@Phone", SqlDbType.NVarChar, -1, "Phone");
+            SqlParameter param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.Direction = ParameterDirection.Output;
+            adapter.InsertCommand = command;
+            // update
+            command = new SqlCommand(@"UPDATE [dbo].[Table] SET FIO = @FIO, Birthday = @Birthday, Email = @Email, Phone = @Phone WHERE ID = @ID", connection);
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@Birthday", SqlDbType.NVarChar, -1, "Birthday");
+            command.Parameters.Add("@Email", SqlDbType.NVarChar, 100, "Email");
+            command.Parameters.Add("@Phone", SqlDbType.NVarChar, -1, "Phone");
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand = command;
+            //delete
+            command = new SqlCommand("DELETE FROM [dbo].[Table] WHERE ID = @ID", connection);
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.DeleteCommand = command;
+            dt = new DataTable();
+            adapter.Fill(dt);
+            //dt.DefaultView.RowFilter = "FIO='My'";//Позволяет настроить DefaultView
+            dt.DefaultView.Sort = "FIO DESC, Email ASC";
+            peopleDataGrid.DataContext = dt.DefaultView;
+        }
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            //добавим новую строку
+           DataRow newRow = dt.NewRow();
+            EditWindow editWindow = new EditWindow(newRow);
+            editWindow.ShowDialog();
+            if (editWindow.DialogResult.HasValue && editWindow.DialogResult.Value)
+            {
+                dt.Rows.Add(editWindow.resultRow);
+                adapter.Update(dt);
+            }
+        }
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)peopleDataGrid.SelectedItem;
+            newRow.BeginEdit();
+            EditWindow editWindow = new EditWindow(newRow.Row);
+            editWindow.ShowDialog();
+            if (editWindow.DialogResult.HasValue && editWindow.DialogResult.Value)
+            {
+                newRow.EndEdit();
+                adapter.Update(dt);
+            }
+            else
+            {
+                newRow.CancelEdit();
+            }
+        }
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)peopleDataGrid.SelectedItem;
+
+            newRow.Row.Delete();
+            adapter.Update(dt);
+        }
+
+
+    }
+}
---
+++ SimpleDataBaseExample_NetCore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace SimpleDataBaseExample
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExampleDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            //SqlCommand command2 = new SqlCommand("INSERT INTO [dbo].[Table] (vcPassword, vcLogin) VALUES ('Petrov', '321')",connection);
+            //SqlCommand command2 = new SqlCommand(@"UPDATE [dbo].[Table] SET vcLogin='Login', vcPassword='Password' WHERE ID = 1", connection);
+            SqlCommand command2 = new SqlCommand("DELETE FROM [dbo].[Table] WHERE ID = 1", connection);
+            int reader2 = command2.ExecuteNonQuery();
+            SqlCommand command = new SqlCommand("SELECT Id,vcLogin,vcPassword FROM [dbo].[Table]", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())//Считываем каждую строчку
+                Console.WriteLine("{0}\t{1}\t{2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
